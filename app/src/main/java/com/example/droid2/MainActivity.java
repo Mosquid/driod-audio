8002001cd1cf33893b0e8415edef0fca2d94c2bc
Dmitry Mosquid <mosquid@gmail.com>
2022-10-17 13:53:29 +0300

    first commit

---
+++ app/src/main/java/com/example/droid2/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.droid2;
+
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.droid2.databinding.ActivityMainBinding;
+import com.koushikdutta.async.AsyncServer;
+import com.koushikdutta.async.callback.CompletedCallback;
+import com.koushikdutta.async.http.WebSocket;
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private List<WebSocket> _sockets = new ArrayList<WebSocket>();
+
+
+    private final int duration = 3; // seconds
+    private final int sampleRate = 44100;
+    private  final int port = 1488;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private final double freqOfTone = 840; // hz
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    private void setIp() {
+        Context ctx = getApplicationContext();
+        WifiManager wm = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+
+        View main = findViewById(R.id.mainLayout);
+        TextView ipText = main.findViewById(R.id.IpText);
+        ipText.setText(ip + ":" + port);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        AsyncHttpServer httpServer = new AsyncHttpServer();
+
+        httpServer.listen(AsyncServer.getDefault(), port);
+        httpServer.websocket("/", new AsyncHttpServer.WebSocketRequestCallback() {
+            @Override
+            public void onConnected(final WebSocket webSocket, AsyncHttpServerRequest request) {
+                _sockets.add(webSocket);
+
+                webSocket.setClosedCallback(new CompletedCallback() {
+                    public void onCompleted(Exception ex) {
+                        System.out.println("close");
+                        try {
+                            if (ex != null)
+                                Log.e("WebSocket", "An error occurred", ex);
+                        } finally {
+                            _sockets.remove(webSocket);
+                        }
+                    }
+                });
+
+                webSocket.setStringCallback(new WebSocket.StringCallback() {
+                    @Override
+                    public void onStringAvailable(String s) {
+                        onMessage();
+                        webSocket.send("received");
+                    }
+                });
+
+            }
+        });
+
+        setContentView(binding.getRoot());
+        setIp();
+    }
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    private void onMessage() {
+        genTone();
+        int bufsize = AudioTrack.getMinBufferSize(44100,
+                AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        byte[] b = new byte[20];
+        new Random().nextBytes(b);
+
+        AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC,
+                44100,
+                AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufsize,
+                AudioTrack.MODE_STREAM );
+        audio.write(generatedSnd, 0, generatedSnd.length);
+        audio.play();
+    }
+}
